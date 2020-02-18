638fc59b30b42e9a8ad88503ad319f06dfdd82b8
boec_mt <Mathias.Boeck@dlr.de>
2020-02-18 09:06:32 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # different-scope-for-npm-ghp
+
+This is a test for publishing a npm package to the *GitHub Package Registry*  under a different scope as *@owner*
---
+++ index.js
@@ -0,0 +1,9 @@
+class Test {
+    scope = `@foo`;
+    showScope(){
+        console.log(`This package is published under the scope: ${scope}`);
+    }
+
+}
+
+export { Test }; 
